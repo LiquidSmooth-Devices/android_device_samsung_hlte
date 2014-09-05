6c0bbe19025c535e09e6423146029382d7440398
itsmikeramsay <ronaldramsayii@gmail.com>
2014-09-05 12:59:10 -0400

    BoardConfig: Move kernel variant config
    Change-Id: I5e2d801318c1a45a9b9866e0a4f8bf2ab315774a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,9 @@
 
 TARGET_OTA_ASSERT_DEVICE := hltexx,SM-N9005,hltetmo,SM-N900T,hlteatt,SM-N900A,hlte
 
+# Kernel
+TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_eur_defconfig
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/hlte/init/init_hlte.c
