6bd12ffddd1ccb381530a6105746ccba0ef541db
itsmikeramsay <ronaldramsayii@gmail.com>
2014-09-23 14:07:22 -0400

    hlte: Add proper defconfig
    Change-Id: I9379f0f8b6344f21c42d3fac52c986605c60c9fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,8 @@
 
 TARGET_OTA_ASSERT_DEVICE := hltexx,SM-N9005,hltetmo,SM-N900T,hltecan,SM-N900W8,hlteatt,SM-N900A,hlte
 
+TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_eur_defconfig
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/hlte/init/init_hlte.c
